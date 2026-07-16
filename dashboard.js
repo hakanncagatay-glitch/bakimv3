f425ba25665a4ed608ea81164042476463771757
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-07-16 16:11:40 +0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,37 +1,33 @@
+alert("dashboard.js yüklendi");
+
 const API = "https://script.google.com/macros/s/AKfycbyp4Dexvk759RdZEEdAIS-urDlkJR9-r39_r_gb1w13eidoSpePkYX-6sUBYYZRdCu6ng/exec";
 
 window.onload = dashboardYukle;
 
 async function dashboardYukle() {
 
+    alert("dashboardYukle başladı");
+
     try {
 
         const response = await fetch(API + "?action=dashboardOzet");
 
+        alert("HTTP: " + response.status);
+
         const sonuc = await response.json();
 
-        if (!sonuc.success) {
-
-            alert(sonuc.message);
-
-            return;
-
-        }
+        alert(JSON.stringify(sonuc));
 
         const d = sonuc.data;
 
         document.getElementById("toplamMakine").innerText = d.toplamMakine;
-
         document.getElementById("toplamBakim").innerText = d.toplamBakim;
-
         document.getElementById("yaklasanBakim").innerText = d.yaklasanBakim;
-
         document.getElementById("gecikenBakim").innerText = d.gecikenBakim;
 
-    }
+    } catch (err) {
 
-    catch (err) {
-
+        alert(err);
         console.error(err);
 
     }
