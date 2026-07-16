54357ca62f0ea4354819b12541601de3bc0d619b
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-07-16 11:22:46 +0300

    Display today's date in Turkish format
    Add functionality to display today's date in Turkish format

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,14 @@
+const tarih = new Date();
 
+document.getElementById("today").innerHTML =
+tarih.toLocaleDateString("tr-TR",{
+
+weekday:"long",
+
+day:"numeric",
+
+month:"long",
+
+year:"numeric"
+
+});
