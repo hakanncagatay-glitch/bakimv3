38b0f2eba875a43918d07e885da830fe1418de77
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-07-17 11:29:45 +0300

    Implement machine card display in makineler.js
    Added HTML structure for machine cards with details.

--- makineler.js
+++ makineler.js
@@ -1,1 +1,67 @@
+window.onload = () => {
 
+    const alan = document.getElementById("makineListe");
+
+    alan.innerHTML = `
+
+<div class="makineKart">
+
+<div class="makineUst">
+
+<div class="makineKod">
+
+A001
+
+</div>
+
+<div class="periyot">
+
+90 Gün
+
+</div>
+
+</div>
+
+<div class="makineModel">
+
+BROTHER S7300A
+
+</div>
+
+<div class="makineBilgi">
+
+<i class="fa-solid fa-location-dot"></i>
+
+FABRİKA
+
+</div>
+
+<div class="makineBilgi">
+
+<i class="fa-solid fa-calendar-check"></i>
+
+Son Bakım : 16.07.2026
+
+</div>
+
+<div class="makineBilgi">
+
+<i class="fa-solid fa-calendar-days"></i>
+
+Sonraki Bakım : 14.10.2026
+
+</div>
+
+<div class="detayBtn">
+
+Detay
+
+<i class="fa-solid fa-arrow-right"></i>
+
+</div>
+
+</div>
+
+`;
+
+}
