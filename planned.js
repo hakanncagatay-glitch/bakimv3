ddc1b4e2a91573a520fa879aa68c2d973ffb2f20
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-07-28 15:42:30 +0300

    Update planned.js

--- planned.js
+++ planned.js
@@ -1,1 +1,48 @@
+// ==========================
+// PLANLI BAKIMLAR
+// ==========================
 
+const planliBakimlar = [
+
+    {
+        envanter:"A3",
+        marka:"JUKI",
+        model:"DDL8700",
+        konum:"HAT-2",
+        sonBakim:"28.03.2026",
+        sonrakiBakim:"25.07.2026",
+        periyot:120,
+        durum:"geciken",
+        kalan:-5
+    },
+
+    {
+        envanter:"A5",
+        marka:"YUKI",
+        model:"YK5214",
+        konum:"HAT-1",
+        sonBakim:"01.04.2026",
+        sonrakiBakim:"28.07.2026",
+        periyot:120,
+        durum:"bugun",
+        kalan:0
+    },
+
+    {
+        envanter:"A7",
+        marka:"JUKI",
+        model:"D9000",
+        konum:"HAT-4",
+        sonBakim:"05.04.2026",
+        sonrakiBakim:"31.07.2026",
+        periyot:120,
+        durum:"yaklasan",
+        kalan:3
+    }
+
+];
+function planliBakimlariYukle(){
+
+    console.log("Planlı Bakımlar Yükleniyor");
+
+}
