c6bc87bf071d63bbfec596edc410644972d12af4
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-08-12 16:10:03 +0300

    Add raporGrafigiDegistir function to handle chart type

--- reports.js
+++ reports.js
@@ -3,3 +3,11 @@ function raporlariYukle(){
     console.log("Rapor ekranı hazır.");
 
 }
+function raporGrafigiDegistir(){
+
+    const tip =
+        document.getElementById("reportChartType").value;
+
+    console.log("Seçilen grafik:", tip);
+
+}
