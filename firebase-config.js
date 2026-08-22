a465b1512c973f8fe112dc59d6c5a85dca10602a
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-08-22 11:19:59 +0300

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAKoARFQJRMp31F42su0PeyEGh90GWRBg",
+  authDomain: "bakimpro.firebaseapp.com",
+  projectId: "bakimpro",
+  storageBucket: "bakimpro.firebasestorage.app",
+  messagingSenderId: "1014540679469",
+  appId: "1:1014540679469:web:eb594949a3112db533bba4"
+};
