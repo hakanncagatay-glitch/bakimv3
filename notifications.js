6d089f78cf0ef08b6eb3abb34077d39c04e5c628
hakanncagatay-glitch <hakann.cagatay@gmail.com>
2026-08-24 11:35:52 +0300

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -77,8 +77,6 @@ if (oturum) {
         JSON.parse(oturum);
     console.log("FCM KULLANICI:", user);
 console.log("FCM KULLANICI ID:", user.id);
-const user =
-    JSON.parse(oturum);
     const params =
         new URLSearchParams();
 
